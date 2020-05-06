f8befb385ad8788ddb108f3d4d94e0da87a44c08
Ricardo Sacardo <rperezsacardo@gmail.com>
2020-05-06 21:23:30 +0100

    Logic done, working on css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
+const axios = require('axios');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
@@ -18,8 +18,64 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add the route handlers here:
 
+// app.get('/:id', (request, response) => {
+//   const id = request.params.id;
+//   const url = 'http://www.omdbapi.com/?apikey=c90b8787&i=' + id;
+//   axios
+//     .get(url)
+//     .then(result => {
+//       const movie = result.data;
+//       response.render('single', { movie });
+//     })
+//     .catch(error => {
+//       console.log('There was an error loading response from api');
+//       console.log(error);
+//       response.send('There was an error processing your request.');
+//     });
+// });
+
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (request, response) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      response.render('beers', { beers: beersFromApi });
+    })
+    .catch(error => {
+      console.log('There was an error loading response from api');
+      console.log(error);
+      response.send('There was an error processing your request.');
+    });
+});
+
+app.get('/beers/:id', (request, response) => {
+  const id = request.params.id;
+  punkAPI
+    .getBeer(id)
+    .then(beerFromApi => {
+      console.log('Beer from the api', beerFromApi);
+      response.render('singleBeer', { beer: beerFromApi });
+    })
+    .catch(error => {
+      console.log(error);
+      response.send('There was an error processing your request.');
+    });
+});
+
+app.get('/random-beers', (request, response) => {
+  const id = request.params.id;
+  punkAPI
+    .getRandom()
+    .then(beerFromApi => {
+      console.log('Beer from the api', beerFromApi);
+      response.render('random', { beer: beerFromApi });
+    })
+    .catch(error => {
+      console.log(error);
+      response.send('There was an error processing your request.');
+    });
+});
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
